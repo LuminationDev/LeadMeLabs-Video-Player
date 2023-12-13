594da5320a12a1e13219616fd4908a07ce452773
Edward Michael Coad <ecoad@lumination.com.au>
2023-12-13 10:40:37 +1030

    Loop video and sound control
    - Loop the video by default
    - Mute and unmute the video
    
    Both can be controlled through the video player or the tablet controls.

--- LeadMeLabs-VideoPlayer/App.xaml.cs
+++ LeadMeLabs-VideoPlayer/App.xaml.cs
@@ -1,58 +1,58 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Windows;
 using Sentry;
 
-namespace LeadMeLabs_VideoPlayer
+namespace LeadMeLabs_VideoPlayer;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private void Application_Startup(object sender, StartupEventArgs e)
     {
-        private void Application_Startup(object sender, StartupEventArgs e)
+        InitSentry();
+
+        MainWindow wnd = new();
+        wnd.Show();
+        
+        wnd.WindowStyle = WindowStyle.None;
+        wnd.WindowState = WindowState.Maximized;
+        
+        // Start up the pipe server to receive commands from LeadMe Labs
+        Manager.InitialiseManager();
+
+        // Check for any arguments, [0] - generic cmd (always present), [1] - source, [2] - should repeat
+        string[] args = Environment.GetCommandLineArgs();
+
+        if (args.Length > 1)
         {
-            InitSentry();
+            wnd.WindowStyle = WindowStyle.None;
+            wnd.WindowState = WindowState.Maximized;
 
-            MainWindow wnd = new();
-            wnd.Show();
-            
-            // Start up the pipe server to recieve commands from LeadMe Labs
-            Manager.InitialiseManager();
-
-            string[] args = Environment.GetCommandLineArgs();
-
-            if (args.Length > 1)
-            {
-                wnd.WindowStyle = WindowStyle.None;
-                wnd.WindowState = WindowState.Maximized;
-
-                wnd.videoPlayer.Source = new Uri(args[1]);
-                wnd.videoPlayer.Play();
-            }
-
-            if (args.Length > 2)
-            {
-                if (args[2].Equals("true"))
-                {
-                    wnd.IsRepeat = true;
-                }
-            }
+            wnd.VideoPlayer.Source = new Uri(args[1]);
+            wnd.VideoPlayer.Play();
         }
 
-        private void InitSentry()
+        if (args.Length <= 2) return;
+        if (args[2].Equals("true"))
         {
-            SentrySdk.Init(options =>
-            {
-                options.Dsn = "https://9b87fd592fb64ef7ad64c180fc560ef6@o1294571.ingest.sentry.io/4505264495984640";
-                options.Debug = false;
-                options.TracesSampleRate = 0.1;
-
-                // This option is recommended. It enables Sentry's "Release Health" feature.
-                options.AutoSessionTracking = true;
-                options.IsGlobalModeEnabled = true;
-                options.EnableTracing = true;
-            });
+            wnd.IsRepeat = true;
         }
+    }
+
+    private void InitSentry()
+    {
+        SentrySdk.Init(options =>
+        {
+            options.Dsn = "https://9b87fd592fb64ef7ad64c180fc560ef6@o1294571.ingest.sentry.io/4505264495984640";
+            options.Debug = false;
+            options.TracesSampleRate = 0.1;
+
+            // This option is recommended. It enables Sentry's "Release Health" feature.
+            options.AutoSessionTracking = true;
+            options.IsGlobalModeEnabled = true;
+            options.EnableTracing = true;
+        });
     }
 }
--- LeadMeLabs-VideoPlayer/Manager.cs
+++ LeadMeLabs-VideoPlayer/Manager.cs
@@ -8,243 +8,259 @@ using System.Threading.Tasks;
 using System.Windows;
 using Action = leadme_api.Action;
 
-namespace LeadMeLabs_VideoPlayer
+namespace LeadMeLabs_VideoPlayer;
+
+public static class Manager
 {
-    public static class Manager
+    //List of the valid file types to try and load
+    private static readonly List<string> ValidFileTypes = new() { ".mp4" };
+
+    //Path to the specialised LeadMe video folder
+    private static readonly string FolderPath = GetVideoFolder();
+
+    private static string GetVideoFolder()
     {
-        //List of the valid file types to try and load
-        private static List<string> ValidFileTypes = new List<string> { ".mp4" };
-
-        //Path to the specialised LeadMe video folder
-        public static string folderPath = @"C:\Users\ecoad\Videos\Test";
-
-        /// <summary>
-        /// Load any local video files and send them to a ParentPipeServer. Also Start the pipe server for the 
-        /// local applicaiton using the leadme_api.dll.
-        /// </summary>
-        public static void InitialiseManager()
-        {
-            LoadLocalVideoFiles();
-            PipeServer.Run(LogHandler, PauseHandler, ResumeHandler, ShutdownHandler, DetailsHandler, ActionHandler);
-        }
-
-        #region Pipe Server
-        /// <summary>
-        /// Stop the current pipe server, wait for a set period and then restart the server.
-        /// </summary>
-        public static void RestartPipeServer()
-        {
-            PipeServer.Close();
-
-            Task.Delay(2000).Wait();
-
-            InitialiseManager();
-        }
-
-        /// <summary>
-        /// Handle any error messages from the internal pipe server.
-        /// </summary>
-        private static void LogHandler(string message)
-        {
-            //For now just disregard as there is nowhere to display the messages.
-            Trace.WriteLine(message);
-        }
-
-        /// <summary>
-        /// Pause the MediaPlayer, if there is no source set this will do nothing.
-        /// </summary>
-        private static void PauseHandler()
-        {
-            Application.Current.Dispatcher.Invoke(delegate
-            {
-                MainWindow.MediaElementInstance.Pause();
-            });
-        }
-
-        /// <summary>
-        /// Resume the MediaPlayer, if there is no source set this will do nothing.
-        /// </summary>
-        private static void ResumeHandler()
-        {
-            Application.Current.Dispatcher.Invoke(delegate
-            {
-                MainWindow.MediaElementInstance.Play();
-            });
-        }
-
-        /// <summary>
-        /// Quit the current application. Performing any checks or required functions before exiting.
-        /// </summary>
-        private static void ShutdownHandler()
-        {
-            Application.Current.Dispatcher.Invoke(delegate
-            {
-                Application.Current.Shutdown();
-            });
-        }
-
-        private static void DetailsHandler()
-        {
-
-        }
-
-        /// <summary>
-        /// Perform an action that has been received from the pipe server. This could be a media action or 
-        /// setting a new source.
-        /// </summary>
-        /// <param name="message">A string containing action space and a value separated by a ':'</param>
-        private static void ActionHandler(string message)
-        {
-            string[] tokens = message.Split(',', 2);
-
-            if (tokens.Length < 2)
-                return;
-
-            string action = tokens[1];
-
-            Application.Current.Dispatcher.Invoke(delegate
-            {
-                switch (tokens[0])
-                {
-                    case "media":
-                        HandleMediaAction(action);
-                        break;
-
-                    case "source":
-                        LoadSource(action);
-                        break;
-
-                    case "window":
-                        HandleWindowAction(action);
-                        break;
-                }
-            });
-        }
-
-        /// <summary>
-        /// Handle an action that is related to the MediaElement within the Main window.
-        /// </summary>
-        private static void HandleMediaAction(string action)
-        {
-            switch (action)
-            {
-                case "stop":
-                    MainWindow.MediaElementInstance.Stop();
-                    MainWindow.MediaElementInstance.Source = null;
-                    break;
-                case "repeat":
-                    MainWindow.MainWindowInstance.IsRepeat = !MainWindow.MainWindowInstance.IsRepeat;
-                    break;
-            }
-        }
-
-        /// <summary>
-        /// Change the current source of the MediaElement to the supplied one.
-        /// </summary>
-        public static void LoadSource(string selectedFilePath)
-        {
-            try
-            {
-                MainWindow.MediaElementInstance.Position = TimeSpan.Zero;
-                MainWindow.MediaElementInstance.Source = new Uri(selectedFilePath);
-                MainWindow.MediaElementInstance.Play();
-            }
-            catch (Exception ex)
-            {
-                SentrySdk.CaptureMessage("Unable to load file from (" + selectedFilePath + "), Error: " + ex);
-            }
-        }
-
-        /// <summary>
-        /// Handle an action that is related to the Main window of the application.
-        /// </summary>
-        private static void HandleWindowAction(string action)
-        {
-            switch (action)
-            {
-                case "fullscreen":
-                    Fullscreen();
-                    break;
-            }
-        }
-
-        /// <summary>
-        /// Toggle the main window between full screen and normal. Depending on what the lastest value of the
-        /// window state is.
-        /// </summary>
-        public static void Fullscreen()
-        {
-            if (Application.Current.MainWindow.WindowState == WindowState.Normal)
-            {
-                Application.Current.MainWindow.WindowStyle = WindowStyle.None;
-                Application.Current.MainWindow.WindowState = WindowState.Maximized;
-                Application.Current.MainWindow.Activate();
-            }
-            else
-            {
-                Application.Current.MainWindow.WindowState = WindowState.Normal;
-                Application.Current.MainWindow.WindowStyle = WindowStyle.SingleBorderWindow;
-            }
-        }
-        #endregion
-
-        #region Setup
-        //A Details class that represents all the information about the experience. This can be sent over
-        //a network as a string with the use of the Details Serialize function.
-        private static Details details = new Details
-        {
-            name = "Video Player",
-            globalActions = new List<GlobalAction>
-            {
-                new GlobalAction { name = "Pause", trigger = "pause" },
-                new GlobalAction { name = "Resume", trigger = "resume" },
-                new GlobalAction { name = "Shutdown", trigger = "shutdown" }
-            },
-            levels = new List<Level>
-            {
-                new Level
-                {
-                    name = "Options",
-                    trigger = "",
-                    actions = new List<Action>
-                    {
-                        new Action { name = "Fullscreen", trigger = "window,fullscreen" },
-                        new Action { name = "Repeat", trigger = "media,repeat" }
-                    }
-                },
-                new Level
-                {
-                    name = "Sources",
-                    trigger = "",
-                    actions = new List<Action>()
-                }
-            }
-        };
-
-        /// <summary>
-        /// Load any video files that are in the local Video folder, adding these to the details object
-        /// before sending the details object to LeadMe Labs.
-        /// </summary>
-        private static async void LoadLocalVideoFiles()
-        {
-            string[] files = Directory.GetFiles(folderPath);
-
-            foreach (string filePath in files)
-            {
-                string fileName = Path.GetFileName(filePath);
-                if (ValidFileTypes.Contains(Path.GetExtension(filePath)))
-                {
-                    // Add to the details being sent to LeadMe
-                    details.levels[1].actions.Add(new Action { name = fileName, trigger = $"source,file://{filePath}" });
-                }
-            }
-
-            // Wait while LeadMe updates the game name
-            await Task.Delay(3000);
-
-            // Send the experience details on start up
-            ParentPipeClient.Send(LogHandler, Details.Serialize(details));
-        }
-        #endregion
+        string videosFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos);
+        
+        return videosFolderPath;
     }
+
+    /// <summary>
+    /// Load any local video files and send them to a ParentPipeServer. Also Start the pipe server for the 
+    /// local application using the leadme_api.dll.
+    /// </summary>
+    public static void InitialiseManager()
+    {
+        LoadLocalVideoFiles();
+        PipeServer.Run(LogHandler, PauseHandler, ResumeHandler, ShutdownHandler, DetailsHandler, ActionHandler);
+    }
+
+    #region Pipe Server
+    /// <summary>
+    /// Stop the current pipe server, wait for a set period and then restart the server.
+    /// </summary>
+    public static void RestartPipeServer()
+    {
+        PipeServer.Close();
+
+        Task.Delay(2000).Wait();
+
+        InitialiseManager();
+    }
+
+    /// <summary>
+    /// Handle any error messages from the internal pipe server.
+    /// </summary>
+    private static void LogHandler(string message)
+    {
+        //For now just disregard as there is nowhere to display the messages.
+        Trace.WriteLine(message);
+    }
+
+    /// <summary>
+    /// Pause the MediaPlayer, if there is no source set this will do nothing.
+    /// </summary>
+    private static void PauseHandler()
+    {
+        Application.Current.Dispatcher.Invoke(delegate
+        {
+            MainWindow.MediaElementInstance.Pause();
+        });
+    }
+
+    /// <summary>
+    /// Resume the MediaPlayer, if there is no source set this will do nothing.
+    /// </summary>
+    private static void ResumeHandler()
+    {
+        Application.Current.Dispatcher.Invoke(delegate
+        {
+            MainWindow.MediaElementInstance.Play();
+        });
+    }
+
+    /// <summary>
+    /// Quit the current application. Performing any checks or required functions before exiting.
+    /// </summary>
+    private static void ShutdownHandler()
+    {
+        Application.Current.Dispatcher.Invoke(delegate
+        {
+            Application.Current.Shutdown();
+        });
+    }
+
+    private static void DetailsHandler()
+    {
+
+    }
+
+    /// <summary>
+    /// Perform an action that has been received from the pipe server. This could be a media action or 
+    /// setting a new source.
+    /// </summary>
+    /// <param name="message">A string containing action space and a value separated by a ':'</param>
+    private static void ActionHandler(string message)
+    {
+        string[] tokens = message.Split(',', 2);
+
+        if (tokens.Length < 2)
+            return;
+
+        string action = tokens[1];
+
+        Application.Current.Dispatcher.Invoke(delegate
+        {
+            switch (tokens[0])
+            {
+                case "media":
+                    HandleMediaAction(action);
+                    break;
+
+                case "source":
+                    LoadSource(action);
+                    break;
+
+                case "window":
+                    HandleWindowAction(action);
+                    break;
+            }
+        });
+    }
+
+    /// <summary>
+    /// Handle an action that is related to the MediaElement within the Main window.
+    /// </summary>
+    private static void HandleMediaAction(string action)
+    {
+        switch (action)
+        {
+            case "stop":
+                MainWindow.MediaElementInstance.Stop();
+                MainWindow.MediaElementInstance.Source = null;
+                break;
+            case "repeat":
+                MainWindow.MainWindowInstance.IsRepeat = !MainWindow.MainWindowInstance.IsRepeat;
+                break;
+            case "mute":
+                MainWindow.MainWindowInstance.IsMuted = true;
+                break;
+            case "unmute":
+                MainWindow.MainWindowInstance.IsMuted = false;
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Change the current source of the MediaElement to the supplied one.
+    /// </summary>
+    private static void LoadSource(string selectedFilePath)
+    {
+        try
+        {
+            MainWindow.MediaElementInstance.Position = TimeSpan.Zero;
+            MainWindow.MediaElementInstance.Source = new Uri(selectedFilePath);
+            MainWindow.MediaElementInstance.Play();
+        }
+        catch (Exception ex)
+        {
+            SentrySdk.CaptureMessage("Unable to load file from (" + selectedFilePath + "), Error: " + ex);
+        }
+    }
+
+    /// <summary>
+    /// Handle an action that is related to the Main window of the application.
+    /// </summary>
+    private static void HandleWindowAction(string action)
+    {
+        switch (action)
+        {
+            case "fullscreen":
+                Fullscreen();
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Toggle the main window between full screen and normal. Depending on what the latest value of the
+    /// window state is.
+    /// </summary>
+    private static void Fullscreen()
+    {
+        if (Application.Current.MainWindow == null) return;
+        
+        if (Application.Current.MainWindow.WindowState == WindowState.Normal)
+        {
+            Application.Current.MainWindow.WindowStyle = WindowStyle.None;
+            Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            Application.Current.MainWindow.Activate();
+        }
+        else
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Normal;
+            Application.Current.MainWindow.WindowStyle = WindowStyle.SingleBorderWindow;
+        }
+    }
+    #endregion
+
+    #region Setup
+    //A Details class that represents all the information about the experience. This can be sent over
+    //a network as a string with the use of the Details Serialize function.
+    private static readonly Details Details = new Details
+    {
+        name = "Video Player",
+        globalActions = new List<GlobalAction>
+        {
+            new GlobalAction { name = "Pause", trigger = "pause" },
+            new GlobalAction { name = "Resume", trigger = "resume" },
+            new GlobalAction { name = "Shutdown", trigger = "shutdown" }
+        },
+        levels = new List<Level>
+        {
+            new Level
+            {
+                name = "Options",
+                trigger = "",
+                actions = new List<Action>
+                {
+                    new Action { name = "Fullscreen", trigger = "window,fullscreen" },
+                    new Action { name = "Repeat", trigger = "media,repeat" },
+                    new Action { name = "Mute", trigger = "media,mute" },
+                    new Action { name = "Unmute", trigger = "media,unmute" }
+                }
+            },
+            new Level
+            {
+                name = "Sources",
+                trigger = "",
+                actions = new List<Action>()
+            }
+        }
+    };
+
+    /// <summary>
+    /// Load any video files that are in the local Video folder, adding these to the details object
+    /// before sending the details object to LeadMe Labs.
+    /// </summary>
+    private static async void LoadLocalVideoFiles()
+    {
+        string[] files = Directory.GetFiles(FolderPath);
+
+        foreach (string filePath in files)
+        {
+            string fileName = Path.GetFileName(filePath);
+            if (ValidFileTypes.Contains(Path.GetExtension(filePath)))
+            {
+                // Add to the details being sent to LeadMe
+                Details.levels[1].actions.Add(new Action { name = fileName, trigger = $"source,file://{filePath}" });
+            }
+        }
+
+        // Wait while LeadMe updates the game name
+        await Task.Delay(3000);
+
+        // Send the experience details on start up
+        ParentPipeClient.Send(LogHandler, Details.Serialize(Details));
+    }
+    #endregion
 }
